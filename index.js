8f352da94485f2eab3375b710a76cea22c7b3c6e
Queenadeweb03 <adelekeadeola1430@gmail.com>
2023-09-20 04:01:47 -0700

    task

---
+++ index.js
@@ -0,0 +1,6 @@
+const aboutMe = function (name, weight){
+    prompt('what is your name');
+    return alert(`My name is ${name}, I am weighing ${weight}lbs`);
+
+}
+console.log(aboutMe('Adeola', 35 * 2.2));
